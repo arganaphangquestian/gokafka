2e31175be2104abd6d5245e005ae508dfae90b5c
arganaphangquestian <arganaphangquestian@gmail.com>
2022-07-06 22:30:29 +0700

    add: Consumer

--- consumer/main.go
+++ consumer/main.go
@@ -1,13 +1,33 @@
 package main
 
-import(
+import (
+	"context"
 	"fmt"
-	"time"
+	"log"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+const (
+	TOPIC     = "MESSAGE_TOPIC"
+	PARTITION = 0
 )
 
 func main() {
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", TOPIC, PARTITION)
+	if err != nil {
+		log.Fatal("failed to dial leader:", err)
+	}
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Fatal("failed to close writer:", err)
+		}
+	}()
 	for {
-		fmt.Println("Hello from consumer")
-		time.Sleep(time.Second * 1)
+		msg, err := conn.ReadMessage(10e3)
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Printf("GET MESSAGE: %s\n", string(msg.Value))
 	}
 }
