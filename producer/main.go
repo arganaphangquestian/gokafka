041eefed3aae9494c8e203db489482c08a5adea4
arganaphangquestian <arganaphangquestian@gmail.com>
2022-07-06 22:06:46 +0700

    add: producer

--- producer/main.go
+++ producer/main.go
@@ -1,13 +1,44 @@
 package main
 
-import(
+import (
+	"context"
 	"fmt"
+	"log"
 	"time"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+const (
+	TOPIC     = "MESSAGE_TOPIC"
+	PARTITION = 0
 )
 
 func main() {
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", TOPIC, PARTITION)
+	if err != nil {
+		log.Fatal("failed to dial leader:", err)
+	}
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Fatal("failed to close writer:", err)
+		}
+	}()
+
+	i := 1
 	for {
-		fmt.Println("Hello from producer")
+		send(conn, i)
+		i++
 		time.Sleep(time.Second * 1)
 	}
+}
+
+func send(conn *kafka.Conn, counter int) {
+	_, err := conn.WriteMessages(
+		kafka.Message{Value: []byte(fmt.Sprintf("MESSAGE %d PUSHED!", counter))},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+	log.Println("Message Sent 🎉")
 }
